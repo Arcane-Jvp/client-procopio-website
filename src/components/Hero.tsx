1840e2079676c38cd6dce39e06f62781c907fa72
Marcos Alves <marcos.a.lopes7701@gmail.com>
2025-11-05 23:28:51 +0000

    Remove redundant w-screen from Hero section for improved layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 export default function Hero() {
   return (
-    <section className="h-screen w-screen bg-[#8d0f0f] px-4">
+    <section className="h-screen bg-[#8d0f0f] px-4">
       <Logo className="w-full h-full text-[#9f1313]" ariaLabel="Logo"/>
     </section>
   )
