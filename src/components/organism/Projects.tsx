18e2d990f6bcc306352427187cbe07f3219065bd
Marcos Alves <marcos.a.lopes7701@gmail.com>
2025-11-08 21:07:03 -0300

    Add margin below Projects section for improved spacing

--- src/components/organism/Projects.tsx
+++ src/components/organism/Projects.tsx
@@ -27,7 +27,7 @@ export default function Projects() {
     ];
 
     return (
-        <div className="max-w-400 w-full mx-auto px-5">
+        <div className="max-w-400 w-full mx-auto px-5 mb-10">
             <div className="bg-foreground text-primary rounded-xl w-full p-4 sm:p-6">
                 <h2 id="projects-heading" className="font-title text-3xl mb-6">
                     Alguns projetos
